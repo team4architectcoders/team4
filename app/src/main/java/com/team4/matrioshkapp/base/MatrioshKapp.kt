531a98ba1d858f4be3049a27afd21e95661dd92c
VCANADAO <vcanadao@everis.com>
2019-05-07 09:28:09 +0200

    Remove code

--- app/src/main/java/com/team4/matrioshkapp/base/MatrioshKapp.kt
+++ app/src/main/java/com/team4/matrioshkapp/base/MatrioshKapp.kt
@@ -2,16 +2,12 @@ package com.team4.matrioshkapp.base.di.koin.modules
 
 import android.app.Application
 import android.content.Context
-import androidx.room.Room
-import com.team4.matrioshkapp.base.io.database.MatrioshkappDatabase
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 open class MatrioshKapp : Application() {
     companion object {
-        const val DABASE_NAME = "matrioshkapp_db"
-        lateinit var database: MatrioshkappDatabase
         lateinit var appContext: Context
     }
 
@@ -19,13 +15,8 @@ open class MatrioshKapp : Application() {
         super.onCreate()
         appContext = this.applicationContext
 
-        database = Room.databaseBuilder(
-            this, MatrioshkappDatabase::class.java,
-            DABASE_NAME
-        ).build()
-
         val modules = arrayListOf(
-            iOModule, appModule
+            appModule
         )
 
         startKoin {
--- app/src/main/java/com/team4/matrioshkapp/io/database/MatrioshkappDatabase.kt
+++
@@ -1,6 +0,0 @@
-package com.team4.matrioshkapp.base.io.database
-
-import androidx.room.RoomDatabase
-
-// @Database(entities = [MyEntitiy::class], version = 1, exportSchema = false)
-abstract class MatrioshkappDatabase : RoomDatabase()
